f1a2c3b04cca2ddb90fe6a729fae60ad9db15e7f
Jas Dhaul <jasdhaul0@gmail.com>
2023-03-18 12:44:27 -0700

    Removed old logo's and updated the FormField component

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 
-const FormField = () => {
-  return <div>FormField</div>;
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+}) => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {labelName}
+        </label>
+      </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+      />
+    </div>
+  );
 };
 
 export default FormField;
