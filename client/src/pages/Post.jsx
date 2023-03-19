ff5e0dc3d1a97a096a032067b7d96b834e0eeda7
Jas Dhaul <jasdhaul0@gmail.com>
2023-03-19 13:52:18 -0700

    Finished image generation post API

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -19,7 +19,29 @@ const Post = () => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
 
-  const generateImg = () => {};
+  const generateImg = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch(`http://localhost:8080/api/dalle`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        });
+
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (error) {
+        alert(error);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert("Please enter a prompt");
+    }
+  };
 
   return (
     <section className="mx-auto max-w-7xl">
