963cd48775a29c782af0edfc51f95be90afa99f8
Jas Dhaul <jasdhaul0@gmail.com>
2023-03-19 13:11:18 -0700

    Started implementing backend routes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./mongo/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
 dotenv.config();
 
@@ -9,6 +11,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/posts", postRoutes);
+app.use("/api/dalle", dalleRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
