52e8a2ccedf9cb134afea5e516b9083bccdbe0ec
Jas Dhaul <jasdhaul0@gmail.com>
2023-03-20 21:15:20 -0700

    Finished card layout and functionality

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { AiOutlineCloudDownload } from "react-icons/Ai";
+import { BsFillPersonFill } from "react-icons/Bs";
+import FileSaver from "file-saver";
 
-const Card = () => {
-  return <div>Card</div>;
+const handleImageDownload = (id, link) => {
+  FileSaver.saveAs(link, `image-${id}.jpg`);
+};
+
+const Card = ({ _id, name, prompt, photo }) => {
+  return (
+    <div className="shadow-card h over:shadow-cardhover card group relative rounded-xl">
+      <img
+        className="h-auto w-full rounded-xl object-cover"
+        src={photo}
+        alt={prompt}
+      />
+
+      <div className="absolute bottom-0 left-0 right-0 m-2 hidden max-h-[94.5%] flex-col rounded-md bg-[#10131f] p-4 group-hover:flex">
+        <p className="overflow-y-auto text-sm text-white">{prompt}</p>
+        <div className="mt-5 flex items-center justify-between gap-2">
+          <div className="flex items-center gap-2 text-white">
+            {<BsFillPersonFill />}
+            {name}
+          </div>
+          <AiOutlineCloudDownload
+            color="white"
+            size={30}
+            className="cursor-pointer"
+            onClick={() => handleImageDownload(_id, photo)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,10 +5,14 @@ import Card from "../components/Card";
 import FormField from "../components/FormField";
 
 const RenderCards = ({ data, title }) => {
-  if (data.length > 0) return data.map((post) => <Card key={post._id} />);
+  // Return cards if posts is not empty
+  if (data && data.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />);
+  }
 
+  // Return error text if no posts found
   return (
-    <h2 className="mt-5 text-xl font-bold uppercase text-[#6449ff]">{title}</h2>
+    <h2 className="mt-5 text-xl font-bold uppercase text-[#6469ff]">{title}</h2>
   );
 };
 
@@ -16,6 +20,32 @@ const Home = () => {
   const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
   const [searchText, setSearchText] = useState("");
+
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch("http://localhost:8080/api/posts", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   return (
     <section className="mx-auto mt-16 max-w-7xl">
@@ -49,9 +79,9 @@ const Home = () => {
 
             <div className="sm:grid-col-2 grid gap-3 lg:grid-cols-4">
               {searchText ? (
-                <RenderCards data={[]} title="No search results found" />
+                <RenderCards data={allPosts} title="No search results found" />
               ) : (
-                <RenderCards data={[]} title="No posts found" />
+                <RenderCards data={allPosts} title="No posts found" />
               )}
             </div>
           </div>
