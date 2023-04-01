fcf1477a1d708f2b62d62a7d3cd74a9fb87caa13
Jas Dhaul <jasdhaul0@gmail.com>
2023-04-01 12:42:18 -0700

    Updated build home routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <header className="flex w-full items-center justify-between border-b border-b-[#e6ebf4] bg-white px-4 py-4 sm:px-8">
-        <Link to="/">
+        <Link to="/AI-Imagery">
           <img src={logo} alt="logo" className="w-[175px] object-contain" />
         </Link>
 
@@ -23,7 +23,7 @@ const App = () => {
       </header>
       <main className="min-h-[calc(100vh-73px)] w-full bg-[#f9fafe] px-4 py-8 sm:p-8">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/AI-Imagery" element={<Home />} />
           <Route path="/create" element={<CreatePost />} />
         </Routes>
       </main>
